eba62b77144eeebf682ed77021a5f6256dc38dca
elfico <asamu.a.adebayo@gmail.com>
2024-02-09 09:48:51 +0100

    initial commit

---
+++ SentryExtensionExample/Program.cs
@@ -0,0 +1,77 @@
+
+using NLog.Web;
+using NLog;
+
+namespace SentryExtensionExample
+{
+    public class Program
+    {
+        //public static void Main(string[] args)
+        //{
+        //    var builder = WebApplication.CreateBuilder(args);
+
+        //    // Add services to the container.
+
+        //    builder.Services.AddControllers();
+        //    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        //    builder.Services.AddEndpointsApiExplorer();
+        //    builder.Services.AddSwaggerGen();
+
+        //    var app = builder.Build();
+
+        //    // Configure the HTTP request pipeline.
+        //    if (app.Environment.IsDevelopment())
+        //    {
+        //        app.UseSwagger();
+        //        app.UseSwaggerUI();
+        //    }
+
+        //    app.UseHttpsRedirection();
+
+        //    app.UseAuthorization();
+
+
+        //    app.MapControllers();
+
+        //    app.Run();
+        //}
+
+        public static void Main(string[] args)
+        {
+            var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+
+            try
+            {
+                logger.Debug("init main");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                //NLog: catch setup errors
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                LogManager.Shutdown();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSentry();
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Information);
+                })
+                .UseNLog();  // NLog: Setup NLog for Dependency injection
+        }
+    }
+}
---
+++ SentryExtensionExample/Startup.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Converters;
+using Sentry;
+
+namespace SentryExtensionExample
+{
+    public class Startup
+    {
+        public static readonly LoggerFactory _myLoggerFactory =
+            new LoggerFactory(new[]
+            {
+                new Microsoft.Extensions.Logging.Debug.DebugLoggerProvider()
+            });
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                .AddNewtonsoftJson(x => x.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
+                .AddNewtonsoftJson(x => x.SerializerSettings.Converters.Add(new StringEnumConverter()));
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddCors(option =>
+            {
+                option.AddDefaultPolicy(
+                    builder =>
+                    {
+                        builder
+                        .AllowAnyOrigin()
+                        .SetIsOriginAllowedToAllowWildcardSubdomains()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                    });
+            });
+
+
+            services.AddSignalR();
+            services.AddHttpClient();
+            services.AddHttpContextAccessor();
+
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+
+
+            //initialize sentry sdk
+            SentrySdk.Init(sentry =>
+            {
+                SentryOptions options = new SentryOptions();
+
+                // Add this to the SDK initialization callback
+                // To set a uniform sample rate
+                options.TracesSampleRate = 1.0;
+
+                options.TracesSampler = context =>
+                {
+                    // If this is the continuation of a trace, just use that decision (rate controlled by the caller)
+                    if (context.TransactionContext.IsParentSampled is not null)
+                    {
+                        return context.TransactionContext.IsParentSampled.Value
+                            ? 1.0
+                            : 0.0;
+                    }
+
+                    // Otherwise, sample based on URL (exposed through custom sampling context)
+                    return context.CustomSamplingContext.GetValueOrDefault("url") switch
+                    {
+                        // The websocket request endpoint is just noise - drop all transactions
+                        "/api/notify" => 0.0,
+                        "api/notify/negotiate" => 0.0,
+
+                        // Or return null to fallback to options.TracesSampleRate (1.0 in this case)
+                        _ => null
+                    };
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IHttpContextAccessor httpContext)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                //app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            // Enable automatic tracing integration.
+            // Make sure to put this middleware right after `UseRouting()`.
+            app.UseSentryTracing();
+
+            app.UseCors();//this was moved here since the BasicAuthMiddleware below is also authentication and cors must come before authentication.
+
+            if (env.IsDevelopment() || env.IsStaging())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(opt =>
+                {
+                    opt.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+                });
+            }
+
+            //TODO: Configure api to throw 400 if the calling client is not doing so from https; using .UseHttpsRedirection is suitable for web apps not api
+            //https://docs.microsoft.com/en-us/aspnet/core/security/enforcing-ssl?view=aspnetcore-2.2&tabs=visual-studio
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
